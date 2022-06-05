5f647643b4d979899705f0f1a37192e783b1cc24
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-05 13:27:44 -0700

    Update lc_283_solution.py

--- lc_283_solution.py
+++ lc_283_solution.py
@@ -1,28 +1,33 @@
 #o(n) time and o(n) space(in place) solution
 
-non_zeroes_index = -1 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        non_zeroes_index = -1 
         
-    for i in range(0, len(nums)):
-        if nums[i] != 0: 
-            non_zeroes_index += 1 
-            nums[non_zeroes_index] = nums[i]
+        for i in range(0, len(nums)):
+            if nums[i] != 0: 
+                non_zeroes_index += 1 
+                nums[non_zeroes_index] = nums[i]
        
-    for i in range(non_zeroes_index+1, len(nums)): 
-        nums[i] = 0 
+        for i in range(non_zeroes_index+1, len(nums)): 
+            nums[i] = 0 
+        
 
 
 #o(n) time and o(n) space(not in place) solution 
+"""
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        result = []
+        
+        for i in range (0, len(nums)): 
+            if nums[i] != 0: 
+                result.append(nums[i])
+        for i in range (0, len(nums)): 
+            if nums[i] == 0: 
+                result.append(nums[i])
+        
+        for i in range(0, len(nums)): 
+            nums[i] = result[i]
 
-result = []
-        
-    for i in range (0, len(nums)): 
-        if nums[i] != 0: 
-            result.append(nums[i])
-    for i in range (0, len(nums)): 
-        if nums[i] == 0: 
-            result.append(nums[i])
-        
-    for i in range(0, len(nums)): 
-        nums[i] = result[i]
-
-        
+""" 
