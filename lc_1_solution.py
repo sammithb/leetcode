491f0da5a9854c23453d19c8cbeafc0b92907a6f
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-07 16:53:42 -0700

    Update lc_1_solution.py

--- lc_1_solution.py
+++ lc_1_solution.py
@@ -11,5 +11,19 @@ class Solution:
                 return[key_value_dict[num], i]
             else: 
                 key_value_dict[nums[i]] = i 
+
+
+# O(n^2) time and space solution 
+
+'''
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range (len(nums)): 
+            for j in range(i+1, len(nums)): 
+                sum = nums[i] + nums[j]
+                if sum == target: 
+                    return [i,j]
+
+'''
                 
                 
