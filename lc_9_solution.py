0ef65c51f6172f576f1b3bb664e8d3e118520255
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-07 15:59:16 -0700

    Update lc_9_solution.py

--- lc_9_solution.py
+++ lc_9_solution.py
@@ -8,4 +8,17 @@ class Solution:
             return True 
         else: 
             return False 
-        
+    
+# O(n) time and O(n) space solution without converting to a string 
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = x 
+        rev_num = 0 
+        while num > 0: 
+            rev_num = rev_num * 10 + num%10 
+            num = num // 10 
+        if x == rev_num: 
+            return True 
+        else: 
+            return False 
