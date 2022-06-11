18e92fe82d6a5e53b8eabd13e199edabab2f4a18
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-11 12:55:29 -0700

    Update lc_27_solution.py

--- lc_27_solution.py
+++ lc_27_solution.py
@@ -6,9 +6,13 @@ class Solution:
         
         for i in range(0, len(nums)):
             if nums[i] == val:
+                # checking if the value given is one of the indices in the list nuns 
                 num_count += 1
+                #if you see it increase the final count given, or as shown in the description "K", increase that by 1
                 
             else :
+                #case for if the value is not in the index num
                 nums[i-num_count] = nums[i]
+                #assign the remaining numbers in the array to nums[i], if the val is not already equal to it
                 
         return len(nums) - num_count
