1942feaee8d7ee227326b4ae755164b576d3957a
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-11 15:16:17 -0700

    Update lc_27_solution.py

--- lc_27_solution.py
+++ lc_27_solution.py
@@ -1,18 +1,19 @@
-# Order (n) time and Order (1) space solution 
+# O(n) time and O(1) space solution 
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        num_count = 0
+        val_count = 0
         
         for i in range(0, len(nums)):
+            # If number at index i is equal to val, increment the count 
+            # of the number of times val has been seen
             if nums[i] == val:
-                # checking if the value given is one of the indices in the list nuns 
-                num_count += 1
-                #if you see it increase the final count given, or as shown in the description "K", increase that by 1
-                
+                val_count += 1
+            # if the number at index i is not equal to val 
+            # copy the number at index i to it's left by skipping the count 
+            # of times val has been seen so far
             else :
-                #case for if the value is not in the index num
-                nums[i-num_count] = nums[i]
-                #assign the remaining numbers in the array to nums[i], if the val is not already equal to it
-                
-        return len(nums) - num_count
+                nums[i - val_count] = nums[i]
+        # return the count of values in the list that are not equal to 
+        # val       
+        return len(nums) - val_count
