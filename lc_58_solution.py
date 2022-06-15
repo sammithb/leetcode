51aef11a4a8293e6b134100e0530a8429dabe251
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-15 00:37:46 -0700

    Create lc_58_solution.py

---
+++ lc_58_solution.py
@@ -0,0 +1,25 @@
+# O(n) time and O(1) space solution 
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        index = len(s) - 1
+        
+        while index >= 0 and s[index] == " ": 
+            index -= 1
+        
+        end_index = index 
+        
+        while index >= 0 and s[index] != " ":
+            index -= 1
+            
+        return end_index-index
+        
+'''
+# O(n) time O(n) space
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.strip()
+        
+        words = s.split(" ")
+        
+        return len(words[-1])
+'''
