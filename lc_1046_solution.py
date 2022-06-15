81c886eec89d86cce80dfa95c5e6cee2b0f9ad7c
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-14 23:46:45 -0700

    Update lc_1046_solution.py

--- lc_1046_solution.py
+++ lc_1046_solution.py
@@ -1,5 +1,26 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        for i in range (0, len(stones)): 
+            stones[i] *= -1 
+        
+        heapq.heapify(stones) 
+        
+        while len(stones) > 1: 
+            l1 = heapq.heappop(stones) * -1
+            l2 = heapq.heappop(stones) * -1
+         
+            diff = abs(l1-l2)
+            
+            heapq.heappush(stones, -1 * diff)
+        
+        return -(stones[0])
+
+
+
+# sum from i = 2 to i = n, i log i 
+'''
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
         # sorting has to be used here so any n log n sorting will work
         
         while len(stones) > 1:
@@ -11,3 +32,4 @@ class Solution:
             # it has to be an absolute value because the appended number cannot be negative 
             
         return stones[0]
+'''
