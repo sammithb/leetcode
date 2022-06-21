11e1c0676555bada01f4fdfc806fd3f18c1b8ed0
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-20 18:37:08 -0700

    Update lc_169_solution.py

--- lc_169_solution.py
+++ lc_169_solution.py
@@ -18,3 +18,29 @@ class Solution:
         for num in num_count: 
             if num_count[num] == max_count: 
                 return num 
+
+'''
+# O(n log n) time and O(1) space solution 
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        
+        majority_number = -1 
+        num_count = 0 
+        
+        L = 0 
+        R = 0 
+        
+        while L < len(nums):
+            while R < len(nums) and nums[L] == nums[R]: 
+                R += 1 
+                
+            if R - L > num_count: 
+                num_count = R - L 
+                majority_number = nums[L]
+                
+            L = R 
+                
+        return majority_number 
+'''
