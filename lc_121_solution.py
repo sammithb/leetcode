0262328b55c4467298cd5e2418256ca1d1b450a6
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-29 16:53:07 -0700

    Create lc_121_solution.py

---
+++ lc_121_solution.py
@@ -0,0 +1,21 @@
+# O(n) time and O(n) space solution
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if len(prices) < 2: 
+            return 0
+        
+        max_to_right = prices[:]
+        max_to_right[-1] = 0
+        for i in reversed(range(0, len(prices)-1)):
+            max_to_right[i] = max(prices[i+1], max_to_right[i+1])
+        
+        possible_profits = []
+        for buy in range (0, len(prices)-1): 
+            purchase_price = prices[buy]
+            max_sale_price = max_to_right[buy]
+            possible_profit = max(0, max_sale_price - purchase_price)
+            
+            possible_profits.append(possible_profit) 
+            
+        return max(possible_profits)
