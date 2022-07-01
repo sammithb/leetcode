275eb4d7613a56010a8292cb947f968dbca807c7
sammithb <64885534+sammithb@users.noreply.github.com>
2022-06-30 18:03:45 -0700

    Create lc_561_solution.py

---
+++ lc_561_solution.py
@@ -0,0 +1,10 @@
+# O(n log n) time and O(n) space solution 
+
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        result = 0 
+        for i in range(0, len(nums), 2): 
+            result += min(nums[i], nums[i+1])
+            
+        return result 
